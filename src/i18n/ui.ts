8d297fe7ca33773dfe3f4bfc1cfe83e41ef93255
Diego <juandiegomatute11@gmail.com>
2024-04-08 17:00:20 -0600

    Update images and projects

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.#experience':'Experience',
+      'nav.#projects':'Projects',
+      'nav.#stack':'Stack',
+      'nav.#about':'About me',
+      'nav.#contact':'Contact'
+    },
+    es: {
+        'nav.#experience':'Experiencia',
+        'nav.#projects':'Proyectos',
+        'nav.#stack':'Stack',
+        'nav.#about':'Sobre mi',
+        'nav.#contact':'Contacto'
+    },
+  } as const;
